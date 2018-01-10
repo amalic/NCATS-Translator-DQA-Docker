2c18f16f33da56c7be19886567da45c9016ec687
Alexander Malic <alexander.malic@gmail.com>
2018-01-10 18:10:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,10 @@ service graphdb start
 
 echo waiting for graphdb ...
 while ! echo exit | nc localhost 7200; do sleep 2; done
-echo graphdb finished
+echo graphdb started
+
+export MAVEN_OPTS="-Xmx8G -XX:MaxPermSize=128m" 
+echo Maven options set to $MAVEN_OPTS
 
 echo executing translator_dqa.py $@
 ./translator_dqa.py $@
